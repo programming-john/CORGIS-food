34c2efdb7fe053e3e74e991ec6b71eafac55efb0
programming-john <31251360+programming-john@users.noreply.github.com>
2017-11-13 09:49:59 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -4,9 +4,6 @@ import json
 
 app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
 
-with open('immigration.json') as immigrants_data:
-        data = json.load(immigrants_data)
-
 @app.route("/")
 def render_main():
     return render_template('index.html')
