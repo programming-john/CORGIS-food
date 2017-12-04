1fbcf82b280d33e7272e6ced983f5945463a0e58
Johnny <johnathoncymoore@gmail.com>
2017-12-04 10:01:14 -0800

    memes

--- webapp.py
+++ webapp.py
@@ -4,9 +4,28 @@ import json
 
 app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
 
+with open('food.json') as food_data:
+        data = json.load(food_data)
+
+def get_category():
+    d = data[0]["Category"]
+
 @app.route("/")
 def render_main():
     return render_template('index.html')
 
+@app.route("/vitamins")
+def render_stats():
+    return render_template('vitamins.html')
+
+@app.route("/minerals")
+def render_minerals():
+    return render_template('minerals.html')
+    
+@app.route("/pie")
+def render_pie():
+        return render_template('piechart.html')
+
+
 if __name__=="__main__":
-    app.run(debug=False, port=54321)
+    app.run(debug=True, port=54321)
