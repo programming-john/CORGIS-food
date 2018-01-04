6ff3a06a27ca2b035811b132654250bed7d78f02
Johnny <johnathoncymoore@gmail.com>
2018-01-04 10:57:15 -0800

    Goose

--- webapp.py
+++ webapp.py
@@ -7,25 +7,74 @@ app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  
 with open('food.json') as food_data:
         data = json.load(food_data)
 
-def get_category():
-    d = data[0]["Category"]
+def get_food_names():
+	options = ""
+	
+	for food in data:
+		options += Markup("<option value=" +'"'+ food["Description"] +'">'+ food["Description"] + "</option>")
+    
+	return options
 
+	
+def get_vit_fact(name):
+	fact = ""
+	for food in data:
+		if name == food["Description"]:
+			fact += Markup("<p><b>"+ food["Description"] + "</b></p>")
+			fact += Markup("<p>"+"Vitamin A - IU: " + str(food["Data"]["Vitamins"]["Vitamin A - IU"]) + "</p>")
+			fact += Markup("<p>"+"Vitamin A - RAE: " + str(food["Data"]["Vitamins"]["Vitamin A - RAE"])+"</p>")
+			fact += Markup("<p>"+ "Vitamin B12: " + str(food["Data"]["Vitamins"]["Vitamin B12"]) + "</p>")
+			fact += Markup("<p>"+"Vitamin B6: " + str(food["Data"]["Vitamins"]["Vitamin B6"])+"</p>")
+			fact += Markup("<p>"+"Vitamin C: " + str(food["Data"]["Vitamins"]["Vitamin C"])+"</p>")
+			fact += Markup("<p>"+"Vitamin E: " + str(food["Data"]["Vitamins"]["Vitamin E"])+"</p>")
+			fact += Markup("<p>"+"Vitamin K: " + str(food["Data"]["Vitamins"]["Vitamin K"])+"</p>")
+			
+	return fact
+	
+def get_min_fact(name):
+	fact = ""
+	for food in data:
+		if name == food["Description"]:
+			fact += Markup("<p><b>"+ food["Description"] + "</b></p>")
+			fact += Markup("<p>"+ "Calcium: " + str(food["Data"]["Major Minerals"]["Calcium"]) + "</p>")
+			fact += Markup("<p>"+ "Copper: " + str(food["Data"]["Major Minerals"]["Copper"]) + "</p>")
+			fact += Markup("<p>"+ "Iron: " + str(food["Data"]["Major Minerals"]["Iron"]) + "</p>")
+			fact += Markup("<p>"+ "Magnesium: " + str(food["Data"]["Major Minerals"]["Magnesium"]) + "</p>")
+			fact += Markup("<p>"+ "Phosphorus: " + str(food["Data"]["Major Minerals"]["Phosphorus"]) + "</p>")
+			fact += Markup("<p>"+ "Potassium: " + str(food["Data"]["Major Minerals"]["Potassium"]) + "</p>")
+			fact += Markup("<p>"+ "Sodium: " + str(food["Data"]["Major Minerals"]["Sodium"]) + "</p>")
+			fact += Markup("<p>"+ "Zinc: " + str(food["Data"]["Major Minerals"]["Zinc"]) + "</p>")
+			
+			
+	return fact
+	
 @app.route("/")
 def render_main():
     return render_template('index.html')
 
 @app.route("/vitamins")
 def render_stats():
-    return render_template('vitamins.html')
+    return render_template('vitamins.html', Vitamins = get_food_names())
 
 @app.route("/minerals")
 def render_minerals():
-    return render_template('minerals.html')
+    return render_template('minerals.html', MajorMinerals = get_food_names())
     
 @app.route("/pie")
 def render_pie():
-        return render_template('piechart.html')
+    return render_template('piechart.html', Goose = get_food_names())
 
-
+@app.route("/go", methods=['GET','POST'])
+def render_vitamininfo():
+    place = request.args['vits']
+    return render_template('vitamins.html', info = get_vit_fact(place), Vitamins = get_food_names())
+	
+@app.route("/yargg", methods=['GET','POST'])
+def render_mineralinfo():
+    place = request.args['mins']
+    return render_template('minerals.html', info = get_min_fact(place), MajorMinerals = get_food_names())
+		
+		
+		
 if __name__=="__main__":
     app.run(debug=True, port=54321)
